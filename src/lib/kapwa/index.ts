30cb20afc187527c38f92ba99349f3ce55a6c23e
Nicu Listana <niculistana@gmail.com>
2026-02-20 23:46:02 -0800

    fix: add new components to build

--- src/lib/kapwa/index.ts
+++ src/lib/kapwa/index.ts
@@ -3,5 +3,19 @@ import { Button } from './button';
 import { Card } from './card';
 import { Input } from './input';
 import { Label } from './label';
+import { CivicTechBanner } from './civic-tech-banner';
+import { StripBanner } from './strip-banner';
+import { Table } from './table';
+import { List } from './list';
 
-export { Banner, Button, Card, Input, Label };
+export {
+  Banner,
+  Button,
+  Card,
+  Input,
+  Label,
+  CivicTechBanner,
+  StripBanner,
+  Table,
+  List,
+};
